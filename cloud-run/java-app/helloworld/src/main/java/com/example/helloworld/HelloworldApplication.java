eb6a297584031b5559a643f385c964c7d6f51f87
VIKAS9021 <vikas.b.gorle@gmail.com>
2026-02-26 18:43:16 +0000

    firestore testing

--- cloud-run/java-app/helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ cloud-run/java-app/helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,6 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
 public class HelloworldApplication {
 
@@ -19,9 +34,32 @@ public class HelloworldApplication {
     String hello() {
       return "Hello " + name + "!";
     }
+    @GetMapping("/test")
+    String test() {
+      return "This is an testing endpoint!";
+    }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     SpringApplication.run(HelloworldApplication.class, args);
+
+    FirestoreOptions firestoreOptions =
+    FirestoreOptions.getDefaultInstance().toBuilder()
+        .setProjectId("divine-ceremony-167109")
+        .setDatabaseId("db-test-firestore") 
+        .setCredentials(GoogleCredentials.getApplicationDefault())
+        .build();
+    Firestore db = firestoreOptions.getService();
+
+    // [START firestore_setup_dataset_read]
+    // asynchronously retrieve all users
+    ApiFuture<QuerySnapshot> query = db.collection("tasks").get();
+    // ...
+    // query.get() blocks on response
+    QuerySnapshot querySnapshot = query.get();
+    List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+    for (QueryDocumentSnapshot document : documents) {
+      System.out.println("User: " + document.getId() + " Data" + document.getData());
+    }
   }
 }
